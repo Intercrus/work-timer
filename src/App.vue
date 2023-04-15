19f3c1a4ee5bda2f845246313309f84372683651
Intercrus <squadew@yandex.ru>
2023-04-15 18:16:41 +0300

    Add layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
+  <div class="timer">
+    <div class="timer__layout">
+      <button class="timer__language-btn btn"></button>
+      <button class="timer__theme-btn btn"></button>
+      <div class="timer__display"><span></span></div>
+      <button class="timer__reset-timer-btn btn"></button>
+      <button class="timer__theory-mode-btn"></button>
+      <button class="timer__practice-mode-btn"></button>
+      <button class="timer__start-stop-timer-btn btn"></button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
 }
 </script>
 
