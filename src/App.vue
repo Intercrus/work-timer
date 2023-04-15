6383e8ccce813b912ea848d59cf907ee41dae00e
Intercrus <squadew@yandex.ru>
2023-04-15 19:18:10 +0300

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="timer">
-    <div class="timer__layout">
-      <button class="timer__language-btn btn">
-        <span>RU</span>
-        <span>EN</span>
-      </button>
-      <button class="timer__theme-btn btn">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><rect fill="none" height="24" width="24"/><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"/></svg>
-      </button>
-      <div class="timer__display"><span></span></div>
-      <button class="timer__reset-timer-btn btn">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><g><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><path d="M12,5V1L7,6l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6H4c0,4.42,3.58,8,8,8s8-3.58,8-8S16.42,5,12,5z"/></g></svg>
-      </button>
-      <button class="timer__theory-mode-btn">Theory</button>
-      <button class="timer__practice-mode-btn">Practice</button>
-      <button class="timer__start-stop-timer-btn btn">
-        <!-- This is start icon. -->
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
-        <!-- This is stop icon. -->
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
-      </button>
+    <div class="timer__wrapper">
+      <div class="timer__layout">
+        <button class="timer__language-btn btn">
+          <span>RU</span>
+          <span>EN</span>
+        </button>
+        <button class="timer__theme-btn btn">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><rect fill="none" height="24" width="24"/><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"/></svg>
+        </button>
+        <div class="timer__display"><span>00:00:00</span></div>
+        <button class="timer__reset-timer-btn btn">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><g><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><path d="M12,5V1L7,6l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6H4c0,4.42,3.58,8,8,8s8-3.58,8-8S16.42,5,12,5z"/></g></svg>
+        </button>
+        <button class="timer__theory-mode-btn btn">Theory</button>
+        <button class="timer__practice-mode-btn btn">Practice</button>
+        <button class="timer__start-stop-timer-btn btn">
+          <!-- This is start icon. -->
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
+          <!-- This is stop icon. -->
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -30,4 +32,99 @@ export default {
 </script>
 
 <style>
+/*.dark-theme {*/
+/*  --background-color: #111827;*/
+/*  --accent-color: #EA5455;*/
+/*  --text-color: #ffffff;*/
+/*}*/
+
+:root {
+  --background-color: #ffffff;
+  --accent-color: #EA5455;
+  --text-color: #111827;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.btn {
+  background: var(--background-color);
+  color: var(--text-color);
+  border: 1px solid var(--text-color);
+  padding: 20px 5px;
+}
+
+.btn:hover {
+  background-color: var(--accent-color);
+  cursor: pointer;
+}
+
+.icon {
+  fill: var(--text-color);
+}
+
+.timer__wrapper {
+  position: absolute;
+  width: 600px;
+  margin: auto;
+  top: 20%;
+  right: 0;
+  left: 0;
+}
+
+.timer__layout {
+  display: grid;
+  grid-template-areas:
+    "none none none none none none language theme"
+    "display display display display display display display display"
+    "reset theory theory theory practice practice practice start-stop";
+}
+
+.timer__language-btn {
+  grid-area: language;
+  border-bottom: none;
+  font-size: 20px;
+}
+
+.timer__theme-btn {
+  grid-area: theme;
+  border-left: none;
+  border-bottom: none;
+}
+
+.timer__display {
+  grid-area: display;
+  padding: 100px;
+  border: 1px solid var(--text-color);
+  color: var(--text-color);
+  background: var(--background-color);
+  border-bottom: none;
+  font-size: 100px;
+  letter-spacing: 5px;
+  user-select: none;
+}
+
+.timer__reset-timer-btn {
+  grid-area: reset;
+}
+
+.timer__theory-mode-btn {
+  grid-area: theory;
+  border-left: none;
+  font-size: 20px;
+}
+
+.timer__practice-mode-btn {
+  grid-area: practice;
+  border-left: none;
+  font-size: 20px;
+}
+
+.timer__start-stop-timer-btn {
+  grid-area: start-stop;
+  border-left: none;
+}
 </style>
