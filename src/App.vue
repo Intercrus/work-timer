33fb1b286d28953128a02931c7324b7c94acef9e
Intercrus <squadew@yandex.ru>
2023-04-15 23:52:34 +0300

    Add feature reset timer

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,10 @@
         >
           <span>{{ formattedCounter }}</span>
         </div>
-        <button class="timer__reset-timer-btn btn">
+        <button
+            v-on:click="resetTimer"
+            class="timer__reset-timer-btn btn"
+        >
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><g><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><path d="M12,5V1L7,6l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6H4c0,4.42,3.58,8,8,8s8-3.58,8-8S16.42,5,12,5z"/></g></svg>
         </button>
         <button
@@ -128,8 +131,8 @@ export default {
             this.theoryCounter++
           } else if (this.currentMode === 'practice') {
             if (this.counter === 0) {
-              this.isPaused = true
               clearInterval(this.counter_id)
+              this.isPaused = true
               return null
             }
             this.counter--
@@ -137,23 +140,31 @@ export default {
           }
         }, 1000)
       } else if (!this.isPaused) {
-        this.isPaused = true
         clearInterval(this.counter_id)
+        this.isPaused = true
       }
     },
 
     selectTheoryMode() {
+      clearInterval(this.counter_id)
       this.isPaused = true
       this.currentMode = 'theory'
       this.counter = this.theoryCounter
-      clearInterval(this.counter_id)
     },
 
     selectPracticeMode() {
+      clearInterval(this.counter_id)
       this.isPaused = true
       this.currentMode = 'practice'
       this.counter = this.practiceCounter
+    },
+
+    resetTimer() {
       clearInterval(this.counter_id)
+      this.isPaused = true
+      this.counter = 0
+      this.theoryCounter = 0
+      this.practiceCounter = 0
     },
   },
   computed: {
