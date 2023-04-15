0a29c0a041461d160bb5d7c1f123ff2f545e78e7
Intercrus <squadew@yandex.ru>
2023-04-15 20:39:37 +0300

    Add feature change language

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,32 @@
   >
     <div class="timer__wrapper">
       <div class="timer__layout">
-        <button class="timer__language-btn btn">
-          <span>RU</span>
-          <span>EN</span>
+        <button
+            v-on:click="changeLanguage"
+            class="timer__language-btn btn"
+        >
+          <span
+              v-if="currentLanguage === 'ru'"
+          >
+            {{ $t('change-language-btn-text-ru') }}
+          </span>
+          <span
+              v-if="currentLanguage === 'en'"
+          >
+            {{ $t('change-language-btn-text-en') }}
+          </span>
         </button>
         <button
             v-on:click="changeTheme"
             class="timer__theme-btn btn"
         >
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><rect fill="none" height="24" width="24"/>
-            <!-- Outlined reset icon. -->
+            <!-- Outlined reset icon -->
             <path
                 v-if="currentTheme === 'light'"
                 d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"
             />
-            <!-- Filled reset icon. -->
+            <!-- Filled reset icon -->
             <path
                 v-if="currentTheme === 'dark'"
                 d="M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36c-0.98,1.37-2.58,2.26-4.4,2.26 c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"
@@ -30,12 +41,16 @@
         <button class="timer__reset-timer-btn btn">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><g><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><path d="M12,5V1L7,6l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6H4c0,4.42,3.58,8,8,8s8-3.58,8-8S16.42,5,12,5z"/></g></svg>
         </button>
-        <button class="timer__theory-mode-btn btn">Theory</button>
-        <button class="timer__practice-mode-btn btn">Practice</button>
+        <button class="timer__theory-mode-btn btn">
+          {{ $t('theory-mode-btn-text') }}
+        </button>
+        <button class="timer__practice-mode-btn btn">
+          {{ $t('practice-mode-btn-text') }}
+        </button>
         <button class="timer__start-stop-timer-btn btn">
-          <!-- Start icon. -->
+          <!-- Start icon -->
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
-          <!-- Stop icon. -->
+          <!-- Stop icon -->
           <svg xmlns="http://www.w3.org/2000/svg" class="icon" height="36px" viewBox="0 0 24 24" width="36px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
         </button>
       </div>
@@ -44,16 +59,24 @@
 </template>
 
 <script>
+import {i18n} from "@/main";
+
 export default {
   data() {
     return {
-      currentTheme: "light"
+      currentTheme: "light",
+      currentLanguage: "ru"
     }
   },
   methods: {
     changeTheme() {
       this.currentTheme = this.currentTheme === "light" ? "dark" : "light"
       document.documentElement.className = this.currentTheme === "light" ? "dark-theme" : "light-theme"
+    },
+
+    changeLanguage() {
+      this.currentLanguage = i18n.global.locale.value === "ru" ? "en" : "ru"
+      i18n.global.locale.value = i18n.global.locale.value === "ru" ? "en" : "ru"
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createI18n, useI18n} from "vue-i18n";
 
-createApp(App).mount('#app')
+const messages = {
+    en: {
+        "change-language-btn-text-ru": "RU",
+        "change-language-btn-text-en": "EN",
+        "theory-mode-btn-text": "Theory",
+        "practice-mode-btn-text": "Practice"
+    },
+    ru: {
+        "change-language-btn-text-ru": "РУС",
+        "change-language-btn-text-en": "АНГЛ",
+        "theory-mode-btn-text": "Теория",
+        "practice-mode-btn-text": "Практика"
+    }
+}
+
+export const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages
+})
+
+createApp(App, {
+    setup() {
+        const {t} = useI18n()
+        return {t}
+    }
+}).use(i18n).mount('#app')
